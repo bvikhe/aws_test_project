5021dee0a13b19dbd86cc4be5ef024da2b475124
bvikhe <vikhe.bankat92@gmail.com>
2018-10-21 13:19:01 +0530

    test2

---
+++ terraform/aws/environment/compute/amazonaws/com/variables.tfvars
@@ -0,0 +1,11 @@
+aws_vpc = "main"
+aws_vpc_cidr = "10.1.0.0/16"
+aws_subnets = ["10.1.1.0/24", "10.1.2.0/24" ]
+aws_firewall = ["sg1", "sg2"]
+aws_ips = ["10.1.1.1", "10.1.2.1"]
+aws_user = "user"
+aws_password = "password"
+hostnames = ["test1", "test2"]
+region = "ap-south-1"
+gateway = ""
+aws_ami = "ami-06f1e5aed41471da7"
---
+++ terraform/aws/variables.tf
@@ -0,0 +1,32 @@
+variable "aws_vpc" {}
+
+variable "aws_vpc_cidr" {
+}
+
+variable "aws_subnets" {
+type = "list"
+}
+
+variable "aws_firewall" {
+type = "list"
+}
+
+variable "aws_ips" {
+type = "list"
+}
+
+variable "aws_user" {
+type = "string"
+}
+
+variable "aws_password" {
+type = "string"
+}
+
+variable "hostnames" {
+type = "list"
+}
+
+variable "gateway" {}
+variable "region" {}
+variable "aws_ami" {}
